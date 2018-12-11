207363429a2a27f8dfff931761bf3799c3dc26e0
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2018-12-11 16:54:40 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
 function searchFunction() {
-    var input, filter, ul, li, a, i;
+    var input, filter, ul, li, a, i, ii, tfilter, temp;
     input = document.getElementById('searching');
     filter = input.value.toUpperCase();
     ul = document.getElementById("myUL");
     li = ul.getElementsByTagName('li');
-
-    // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < li.length; i++) {
-      if (li[i].getAttribute('data-search-tags').toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
-      } else {
-        li[i].style.display = "none";
+      li[i].style.display = "none";
+    }
+    // Loop through all list items, and hide those who don't match the search query
+    tfilter = filter;
+    for(ii = 0;ii<20;ii++){
+      temp = tfilter.indexOf(' ');
+      if(temp!=-1){
+        tfilter=tfilter.substring(temp);
+      }else{
+        break;
+      }
+      for (i = 0; i < li.length; i++) {
+        if (li[i].getAttribute('data-search-tags').toUpperCase().indexOf(tfilter) > -1) {
+          li[i].style.display = "";
+        }
       }
     }
-}
+  }
 function clearSearch() {
     var input, filter, ul, li, a, i;
     input = document.getElementById('searching');
