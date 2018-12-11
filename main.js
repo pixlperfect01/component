136d5fe8ab983aa057237bf562b8f64575bffd1c
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2018-12-11 09:11:58 -0600

    Update main.js

--- main.js
+++ main.js
@@ -30,3 +30,13 @@ function clearSearch() {
 function googleTranslateElementInit() {
   new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
 }
+    document.body.style.backgroundColor = sessionStorage.getItem("bg");
+document.body.style.color = sessionStorage.getItem("cc");
+function theme() {
+    "rgb(255, 255, 255)" === sessionStorage.getItem("bg") ? (sessionStorage.setItem("bg", "rgb(6, 23, 37)"),
+    sessionStorage.setItem("cc", "#777")) : null == sessionStorage.getItem("bg") ? (sessionStorage.setItem("bg", "rgb(6, 23, 37)"),
+    sessionStorage.setItem("cc", "#777")) : "rgb(6, 23, 37)" === sessionStorage.getItem("bg") && (sessionStorage.setItem("bg", "rgb(255, 255, 255)"),
+    sessionStorage.setItem("cc", "#333"));
+    document.body.style.backgroundColor = sessionStorage.getItem("bg");
+    document.body.style.color = sessionStorage.getItem("cc")
+}
