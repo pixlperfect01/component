19c0680475adebd13ec339790301af67920fc989
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2018-12-12 02:57:36 +0000

    Update main.js

--- main.js
+++ main.js
@@ -44,27 +44,7 @@ function clearSearch() {
       li[i].style.display = "";
     }
 }
-/*if(var number=getUrlVars()["dark"]){
-  document.getElementById('navbarcol').style.backgoundColor = rgb(7,7,7);
-  document.getElementById('navbarcol').style.color = rgb(102,102,102);
-  ducument.body.style.backgroundColor=rgb(0,0,0);
-  document.body.style.color=rgb(255,255,255);
-  document.getElementsByName('com')[0].style.backgroundColor=rgb(255,255,255);
-  document.getElementsByName('com')[0].style.color=rgb(255,255,255);
-  document.getElementsByName('com')[1].style.backgroundColor=rgb(255,255,255);
-  document.getElementsByName('com')[1].style.color=rgb(255,255,255);
-  document.getElementsByName('com')[2].style.backgroundColor=rgb(255,255,255);
-  document.getElementsByName('com')[2].style.color=rgb(255,255,255);
-  document.getElementsByName('com1')[0].style.backgroundColor=rgb(30,135,240);
-  document.getElementsByName('com1')[0].style.color=rgb(255,255,255);
-}*/
+
 function googleTranslateElementInit() {
   new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
-}
-function getUrlVars() {
-  var vars = {};
-  var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-        vars[key] = value;
-    });
-  return vars;
 }
